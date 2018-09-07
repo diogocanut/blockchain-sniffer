e877f985404d2b69756881518e1116a4da5bba36
diogocanut <diogocanut@hotmail.com>
2018-09-07 20:07:48 -0300

    getaddr

---
+++ getaddresses.py
@@ -0,0 +1,32 @@
+import socket
+
+
+def get_node_addresses():
+
+    dns_seeds = [
+        ("seed.bitcoin.sipa.be", 8333),
+        ("dnsseed.bluematt.me", 8333),
+        ("dnsseed.bitcoin.dashjr.org", 8333),
+        ("seed.bitcoinstats.com", 8333),
+        ("seed.bitnodes.io", 8333),
+        ("bitseed.xf2.org", 8333),
+    ]
+
+    found_peers = []
+    try:
+
+        for (ip_address, port) in dns_seeds:
+
+            for info in socket.getaddrinfo(ip_address, port,
+                                           socket.AF_INET, socket.SOCK_STREAM,
+                                           socket.IPPROTO_TCP):
+
+                found_peers.append((info[4][0], info[4][1]))
+        return found_peers
+    except Exception:
+        return None
+
+peers = get_node_addresses()
+print("address list")
+for peer in peers:
+    print(peer)
